ab216009d1ef1a002f06717ab81c7610bc27aca2
ricerice <1797250474@qq.com>
2023-10-01 17:19:37 +0800

    work3 and work2

--- 并行计算1/并行计算1.cpp
+++ 并行计算1/并行计算1.cpp
@@ -3,18 +3,97 @@
 
 #include <iostream>
 #include <thread>
+#include <string>
+#include <format>
 void executeThread()
 {
 	for(int i = 0; i < 5; i++)
 	std::cout << "Hello Concurrent World from thread "<< std::endl;
 }
+void executeThreadWithSimpleArgs(int vValue , std::string vStr)
+{
+	std::cout << std::format("Arguments passed to child thread: {} and {}/n", vValue, vStr);
+}
+class CWallet {
+public :
+	CWallet():m_money(0){}
+	int getMoney() { return m_money; }
+	int addMoney(int vMoney)
+	{
+		for (int i = 0; i < vMoney; i++)
+		{
+			m_money++;
+		}
+		return m_money;
+	}
+	void addMoneyWithMutex(int vMoney)
+	{
+		m_Mutex.lock();
+		for (int i = 0; i < vMoney; i++)
+		{
+			m_money++;
+		}
+		m_Mutex.unlock();
+	}
+	void addMoneyWithLockGuard(int vMoney)
+	{
+		std::lock_guard<std::mutex> lockGuard(m_Mutex);
+		for (int i = 0; i < vMoney; i++)
+		{
+			m_money++;
+		}
+	}
+private:
+	int m_money;
+	std::mutex m_Mutex;
+};
 int main()
 {
-    std::thread ThreadObject(executeThread);
-	for(int i = 0; i < 5; i++)
-	std::cout<< "Hello Concurrent World from main!" << std::endl;
+    //std::thread ThreadObject(executeThread);//创建线程对象
+	// for(int i = 0; i < 5; i++)
+	//std::cout << "Hello Concurrent World from main!" << std::endl;
+	//ThreadObject.join();
+	// std::cout<< "Exiting from Main Thread" << std::endl;
+	
+	
+	
+	//std::thread ThreadObject(executeThreadWithSimpleArgs,4,std::string("TEST"));//创建线程对象带字符
+	// for(int i = 0; i < 5; i++)
+	// std::cout << "Hello Concurrent World from main!" << std::endl;
+	// ThreadObject.join();
+	// std::cout<< "Exiting from Main Thread" << std::endl;
+	//测试CWallet的两个函数
+	
+	
+	
+	CWallet walletObject;
+	//std::thread ThreadObject(&CWallet::addMoney, &walletObject, 500);//addmoney
+	//测试addMoney
+	
+	
+	//std::thread ThreadObject(&CWallet::addMoneyWith, &walletObject, 500);//addMoneyWith
+	/*for (int i = 0; i < 5; i++)
+		std::cout << "Hello Concurrent World from main!" << std::endl;
 	ThreadObject.join();
-	std::cout<< "Exiting from Main Thread" << std::endl;
+	std::cout << "Final wallet money: " << walletObject.getMoney() << std::endl;*/
+
+
+	//测试addMoneyWithMutex
+	
+	
+	//std::thread ThreadObject(&CWallet::addMoneyWithMutex, &walletObject, 500);//addMoneyWithMutex
+	//for(int i = 0; i < 5; i++)
+	//std::cout<< "Hello Concurrent World from main!" << std::endl;
+	//ThreadObject.join();
+	//std::cout << "Final wallet money: " << walletObject.getMoney() << std::endl;
+	//std::cout<< "Exiting from Main Thread" << std::endl;
+
+	//测试addMoneyWithLockGuard
+	std::thread ThreadObject(&CWallet::addMoneyWithLockGuard, &walletObject, 500);//addMoneyWithLockGuard
+	for (int i = 0; i < 5; i++)
+		std::cout << "Hello Concurrent World from main!" << std::endl;
+	ThreadObject.join();
+	std::cout << "Final wallet money: " << walletObject.getMoney() << std::endl;
 	return 0;
 	//
 }
